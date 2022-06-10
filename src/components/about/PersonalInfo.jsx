2e9a9bed23ab7b406b8fd80f037fdc801e1bcbb9
danielcasta0398 <98791147+danielcasta0398@users.noreply.github.com>
2022-06-10 20:02:37 +0200

    update

---
+++ src/components/about/PersonalInfo.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const personalInfoContent = [
+  { meta: "nombres", metaInfo: "Juan Daniel" },
+  { meta: "apellidos", metaInfo: "Castaño Castañeda" },
+  { meta: "Edad", metaInfo: "23 Años" },
+  { meta: "Nacionalidad", metaInfo: "Colombiana" },  
+  { meta: "Telefono", metaInfo: "+34641009503" },
+  { meta: "Email", metaInfo: "juancastano0398@gmail.com" }, 
+];
+
+const PersonalInfo = () => {
+  return (
+    <ul className="about-list list-unstyled open-sans-font">
+      {personalInfoContent.map((val, i) => (
+        <li key={i}>
+          <span className="title">{val.meta}: </span>
+          <span className="value d-block d-sm-inline-block d-lg-block d-xl-inline-block">
+            {val.metaInfo}
+          </span>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default PersonalInfo;
