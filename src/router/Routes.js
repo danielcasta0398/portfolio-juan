8843229f927f61a6201b13b3888e2e02491b4e38
danielcasta0398 <98791147+danielcasta0398@users.noreply.github.com>
2022-06-10 20:32:50 +0200

    s

--- src/router/Routes.js
+++ src/router/Routes.js
@@ -2,7 +2,7 @@ import React from "react";
 import HomeLight from "../views/all-home-version/HomeLight";
 import HomeDark from "../views/all-home-version/HomeDark";
 import NotFound from "../views/NotFound";
-import { BrowserRouter as Router, Route, Switch, HashRouter } from "react-router-dom";
+import { Route, Switch, HashRouter } from "react-router-dom";
 import ScrollTopBehaviour from "../components/ScrollTopBehaviour";
 import Preview from "../views/Preview";
 
