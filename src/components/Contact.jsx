38d6219a38b7715151e52d2445c0075216d98a63
Juan Daniel Castaño <juancastano0398@gmail.com>
2022-09-09 11:20:17 +0200

    Solucion de errores

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,17 +1,6 @@
 import React from "react";
-import { useForm } from "react-hook-form";
 
 const Contact = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
-
-  const onSubmit = (data, e) => {
-    e.target.reset();
-    console.log("Message submited: " + JSON.stringify(data));
-  };
 
   return (
     <>
--- src/views/all-home-version/HomeDark.jsx
+++ src/views/all-home-version/HomeDark.jsx
@@ -5,7 +5,6 @@ import Index from "../../components/about/index";
 import Address from "../../components/Address";
 import Portfolio from "../../components/portfolio/Portfolio";
 import Blog from "../../components/blog/Blog";
-import Contact from "../../components/Contact";
 import Social from "../../components/Social";
 import "./Dark.css"
 
